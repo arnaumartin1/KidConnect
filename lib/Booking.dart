56936dcaa30b6d859a4d685db7873c7aeedb3181
arnaumartin1 <arnau.martin02@estudiant.upf.edu>
2025-05-19 15:25:08 +0200

    Reservas

--- lib/Booking.dart
+++ lib/Booking.dart
@@ -1,14 +1,15 @@
-
 class Booking {
   final String serviceTitle;
   final String providerName;
   final DateTime date;
   final double price;
+  bool isRated;
 
   Booking({
     required this.serviceTitle,
     required this.providerName,
     required this.date,
     required this.price,
+    this.isRated = false,
   });
 }
